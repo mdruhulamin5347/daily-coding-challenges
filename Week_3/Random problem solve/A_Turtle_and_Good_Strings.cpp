d8307f1784e042e1ab43a819ed26da930762787c
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-04-18 01:27:36 +0600

    solve: turtle ang good string

---
+++ Week_3/Random problem solve/A_Turtle_and_Good_Strings.cpp
@@ -0,0 +1,24 @@
+// https://codeforces.com/contest/2003/submission/316012984
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        char ch[n+5];
+        for(int i=0; i<n; i++){
+            cin >> ch[i];
+        }
+        if(ch[0] == ch[n-1]){
+            cout << "NO" << endl;
+        }
+        else{
+            cout << "YES" << endl;
+        }    
+    }
+    return 0;
+}
