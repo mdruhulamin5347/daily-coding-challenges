bd73d01acc3c11d0b403297915c262c223c18c4b
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-04-18 15:55:13 +0600

    solve : Only Pluses

---
+++ Week_3/Random problem solve/A_Only_Pluses.cpp
@@ -0,0 +1,36 @@
+
+// https://codeforces.com/contest/1992/submission/316069636
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b,c;
+        cin >> a >> b >> c;
+        priority_queue<int,vector<int>,greater<int>> pq;
+        pq.push(a);
+        pq.push(b);
+        pq.push(c);
+        int mx = 0;
+        int run_time = 5;
+        while(run_time){
+            int x = pq.top();
+            pq.pop();
+            mx = max(mx,x);
+            if(x <= mx){
+                x++;
+                run_time--;
+            }            
+            pq.push(x);
+        }
+        long long sum = 1;
+        while(!pq.empty()){
+            sum *= pq.top();
+            pq.pop();
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
