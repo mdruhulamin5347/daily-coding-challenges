4a773290ec6fd14ae8bd580aa924989f8488dd86
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-04-26 23:59:50 +0600

    solve: payment without change

---
+++ week_4/random problem/A_Payment_Without_Change.cpp
@@ -0,0 +1,21 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int q;
+    cin >> q;
+    while(q--){
+        int a,b,n,s;
+        cin >> a >> b >> n >> s;
+        int x = min(a, s / n); 
+        s -= x * n;            
+        
+        if (s <= b)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
