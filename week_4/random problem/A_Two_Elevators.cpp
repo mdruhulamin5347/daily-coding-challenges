8db3b2f60f545924171646876e495ec7876fa579
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-04-28 00:13:19 +0600

    solve: solve two elevators

--- week_4/random problem/A_Two_Elevators.cpp
+++ week_4/random problem/A_Two_Elevators.cpp
@@ -10,22 +10,19 @@ int main(){
         int a,b,c;
         cin >> a >> b >> c;
         int second_lift = abs(abs(b-c) + c) -1 ;
-        int first_lift;
-        if(a==1){
-            first_lift = a;
+        int first_lift = abs(a-1);
+
+
+        if(first_lift==second_lift){
+            cout << '3' << endl;
+        }        
+        else if(first_lift > second_lift){
+            cout << '2' << endl;
         }
         else{
-            first_lift = abs(a-1);
+            cout << '1' << endl;
         }
-        if(first_lift < second_lift){
-            cout << 1 << endl;
-        }
-        else if(first_lift > second_lift){
-            cout << 2 << endl;
-        }
-        else if(first_lift==second_lift){
-            cout << 3 << endl;
-        }
+        
     }
     return 0;
 }
