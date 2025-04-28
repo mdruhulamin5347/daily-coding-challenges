5a8767027aceb314b200b0a0e47f18f40e05134c
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-04-29 01:37:57 +0600

    refactor: improve code structure and readability in A. Jellyfish and Undertale solution

--- A_Jellyfish_and_Undertale.cpp
+++ A_Jellyfish_and_Undertale.cpp
@@ -2,11 +2,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        
+int main() {
+    int t; 
+    cin >> t; 
+    while (t--) {
+        int a, b, n;
+        cin >> a >> b >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+
+        int c = b; 
+        int cnt = 0;
+
+        int i=n - 1;
+        while (i>=0) {
+            if (c == 1) {
+                c = min(c + v[i], a);
+                i--;
+            }
+            cnt += c - 1;
+            c=1;            
+        }
+        cout << cnt + 1 << endl;
     }
     return 0;
 }
