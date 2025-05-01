85a16427c59a6a769537f01b85d5fcfc06c35422
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-05-01 21:08:47 +0600

    feat: add implementation for B - Invert And Equalize problem

---
+++ week_4/random problem/B_Invert_And_Equalize.cpp
@@ -0,0 +1,37 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        
+        int group_cnt_0 = 0, group_cnt_1 = 0;
+        bool flag_0 = false, flag_1 = false;
+
+        for(int i = n-1; i >= 0; i--){        
+            if(s[i] == '0' && flag_0 == false){
+                group_cnt_0++;
+                flag_0 = true;
+                flag_1 = false;
+            }
+            else if(s[i] == '1' && flag_1 == false){
+                group_cnt_1++;
+                flag_0 = false;
+                flag_1 = true;
+            }            
+        }
+        if(s[n-1] == '0'){
+            cout << group_cnt_1 << endl;
+        }
+        else{
+            cout << group_cnt_0 << endl;
+        }
+    }
+    return 0;
+}
