3979ecb468de1f1b23468f671c8ba935040dec6b
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-05-01 23:19:27 +0600

    feat: add implementation for A - Upload More RAM problem

---
+++ week_5/random problem/A_Upload_More_RAM.cpp
@@ -0,0 +1,20 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n,k;
+        cin >> n >> k;
+        int min_second = 1;
+        if(n > 1){
+            min_second = (n - 1) * k;
+            min_second++;
+        }
+        cout << min_second << endl;
+    }
+    return 0;
+}
