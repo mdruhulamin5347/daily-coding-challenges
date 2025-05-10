81a2d9d672b95431553e3e5ce72d462bb6fc90c2
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-05-11 01:01:01 +0600

    feat: add implementation for C - Number of Equal problem

---
+++ week_4/module base problem/B_Number_of_Smaller.cpp
@@ -0,0 +1,34 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,m;
+    cin >> n >> m;
+    vector<int> v1(n);
+    vector<int> v2(m);
+    for(int i=0; i<n; i++){
+        cin >> v1[i];
+    }
+    for(int i=0; i<m; i++){
+        cin >> v2[i];
+    }
+
+    int l=0,r=0,cnt = 0;
+    while(r<m){
+        if(l < n && v1[l] < v2[r]){
+            cnt++,l++;      
+        }
+        else{
+            cout << cnt << " ";
+            r++;            
+        }      
+    }
+    
+    return 0;
+}
