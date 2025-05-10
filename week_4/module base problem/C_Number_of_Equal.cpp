ac25c6c600587fd7b0d5c6b6b243e0a6b60c627c
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-05-11 01:28:07 +0600

    feat: add implementation for C - Number of Equal problem

---
+++ week_4/module base problem/C_Number_of_Equal.cpp
@@ -0,0 +1,42 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(n), b(m);
+
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for(int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+
+    long long ans = 0;
+    int l = 0,r = 0;
+    while( l < n && r < m){
+        int cnt1 = 0, cnt2 = 0;
+        int cur = a[l];
+        while(l < n && a[l] == cur){
+            cnt1++,l++;
+        }
+        while(r < m && b[r] < cur){
+            r++;
+        }
+        while(r < m && b[r] == cur){
+            cnt2++,r++;
+        }
+        ans += (1LL * cnt1 * cnt2);
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
