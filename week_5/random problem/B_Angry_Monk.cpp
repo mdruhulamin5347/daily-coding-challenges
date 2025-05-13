bcf1c1838b20dc95431ee7bc90b77d2ed9be18a2
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-05-13 23:18:31 +0600

    feat: add implementation for B. Angry Monk problem

---
+++ week_5/random problem/B_Angry_Monk.cpp
@@ -0,0 +1,35 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >>t;
+    while(t--){
+        int n,k;
+        cin >> n >> k;
+        vector<int>a(k);
+        for(int i = 0;i<k;i++){
+            cin >> a[i];
+        }
+        sort(a.begin(),a.end(),greater<int>());
+        int ans = 0;
+        for(int i = 1;i<k;i++){
+            if(a[i] > 1){
+                ans += (a[i] -1) + a[i];
+            }
+            else{
+                ans++;
+            }
+        }
+        cout << ans <<endl;
+    }
+
+
+    return 0;
+}
