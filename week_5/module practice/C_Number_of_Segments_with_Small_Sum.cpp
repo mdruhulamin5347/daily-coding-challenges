a0f6707ea9c35a31a942655a0edeece2bbb01b58
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-05-21 00:45:19 +0600

    feat: implement C. Number of Segments with Small Sum problem

---
+++ week_5/module practice/C_Number_of_Segments_with_Small_Sum.cpp
@@ -0,0 +1,42 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int n;
+    long long k;
+    cin >> n >> k;
+    vector<int>v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+
+
+    long long sum = 0;
+    long long ans = 0;
+    long long l = 0, r = 0;
+
+    while(r < n){
+        sum += v[r];
+        if(sum <= k){
+            ans += (r - l + 1);
+        }
+        else{
+            while(sum > k){
+                sum -= v[l];
+                l++;
+            }
+            if(sum <= k){
+                ans += (r - l + 1);
+            }
+        }
+        r++;
+    }
+    cout << ans << endl;
+    return 0;
+}
