3bd51fd7986ed749d5a8590e2cd35fbdca6cb77a
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-05-27 23:56:13 +0600

    style: add initial implementation of D - Four Digits problem

---
+++ week_1/D_Four_Digits.cpp
@@ -0,0 +1,17 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string s;
+    cin >> s;
+
+    while(s.size() != 4){
+        cout << s.size();
+    }
+    cout << s;
+    
+    return 0;
+}
