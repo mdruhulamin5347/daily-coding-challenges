6618533f6b5f51b0a069555a0d0110866901cc2c
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-06-02 00:43:00 +0600

    feat: add builtin function implementation for bit manipulation

---
+++ week_7/Bit Oparetor Practice/builtin_function.cpp
@@ -0,0 +1,13 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cout << __builtin_popcount(93) << " "; // Count of set bits in 5
+    cout << __lg(93) << " "; // Log base 2 of 16
+    
+    return 0;
+}
