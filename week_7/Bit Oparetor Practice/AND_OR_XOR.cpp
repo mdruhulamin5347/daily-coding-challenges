3725f381a504514df6a3ba0d90b8761913228a8e
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-06-01 23:09:20 +0600

    style: add problem statements for CodeChef - START188D challenges and implement AND_OR_XOR functionality

---
+++ week_7/Bit Oparetor Practice/AND_OR_XOR.cpp
@@ -0,0 +1,17 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int a ,b;
+    cin >> a >> b;
+
+    cout << (a & b) << " " << (a | b) << " " << (a ^ b) << endl;
+    cout << (~a) << " " << (~b) << endl;
+    
+    return 0;
+}
