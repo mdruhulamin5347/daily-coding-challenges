2e55258fac3f86a521dfae74dea874f86060e941
Md.Ruhul Amin <135340696+mdruhulamin5347@users.noreply.github.com>
2025-06-06 20:48:42 +0600

    Update B_Fun_Game.cpp

--- week_7/Module Base Problem/B_Fun_Game.cpp
+++ week_7/Module Base Problem/B_Fun_Game.cpp
@@ -5,3 +5,10 @@ for i in ruhul:
     print("vacation date->",i)
 print("end")
 print("today")
+    
+ruhul = [6,13,21,29]
+
+for i in ruhul:
+    print("vacation date->",i)
+print("end")
+print("today")
