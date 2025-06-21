f8332e076bc95303142af849d6d2e8c69d6386c9
Md.Ruhul Amin <135340696+mdruhulamin5347@users.noreply.github.com>
2025-06-21 22:05:11 +0600

    Update E_Segments_with_Small_Set.cpp

--- week_5/module practice/E_Segments_with_Small_Set.cpp
+++ week_5/module practice/E_Segments_with_Small_Set.cpp
@@ -1,4 +1,4 @@
-
+y
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -6,7 +6,9 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
- 
+    int n;
+    cin >> n;
+    
     
     return 0;
 }
