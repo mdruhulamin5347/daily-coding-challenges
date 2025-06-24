0a46612c30b3bbf4cfaa084e2e007fc0fbf582af
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-06-24 08:20:35 +0600

    feat: implement E. Segments with Small Set problem and add test cases

--- week_5/module practice/E_Segments_with_Small_Set.cpp
+++ week_5/module practice/E_Segments_with_Small_Set.cpp
@@ -6,7 +6,32 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
- 
-    
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    map<int, int> freq;
+    long long count = 0;
+    int l = 0;
+
+    for(int r = 0; r < n; r++){
+        freq[v[r]]++;
+
+        while(freq.size() > k){
+            freq[v[l]]--;
+            if(freq[v[l]] == 0){
+                freq.erase(v[l]);
+            }
+            l++;
+        }
+
+        count += (r - l + 1);
+    }
+
+    cout << count << endl;
     return 0;
 }
